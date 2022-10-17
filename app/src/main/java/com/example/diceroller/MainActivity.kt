e3fc925f2f28c2659d1e401bbf439fcc59a81e61
Paolo Caldera <paolo.caldera@keypartner.com>
2022-10-17 18:16:10 +0200

    Add button onClick logic

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.diceroller.data.Dice
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Inflate the layout
         setContentView(R.layout.activity_main)
+
+        // Reference in the code of the UI elements
+        val rollButton = findViewById<Button>(R.id.button)
+        val resultTextView = findViewById<TextView>(R.id.textView)
+
+        // Action to perform on button click
+        rollButton.setOnClickListener {
+            resultTextView.text = Dice(6).roll().toString()
+        }
     }
 }
